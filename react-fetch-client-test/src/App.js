4a0ede20e9ad503970e4546d2dd039b8d127ba97
Justin Joco <justin.joco@gmail.com>
2024-05-27 22:18:44 -0700

    adds more notes on react hookos

--- react-fetch-client-test/src/App.js
+++ react-fetch-client-test/src/App.js
@@ -8,6 +8,12 @@ https://httpbin.org/delete
 
 GET https://randomuser.me/api
 
+const [someState, setSomeState] = useState(initialValue) -> setSomeState causes rendering
+
+const someRef = useRef(initialValue) -> not needed for rendering
+oldValue = someRef.current -> reads current value of reference
+someRef.current = newValue -> set new value of reference
+
 Props: passes data and callback functions to different components
 State: real-time data to use in current component
 
@@ -15,9 +21,16 @@ useEffect: triggers after a render (do not let a ref be a dependency) -> use to 
 - Only use setState inside if the useEffect has empty dependencies (eg. there's only data fetching when the component first mounts); otherwise, an infinite loop might happen
 - Cleanup useEffect in the return () => {do something}
 - Cleanup is ran on old props and state, setup is ran on new props and state
+useEffect (() => {
+  setup code
+}, [list of dependencies])
+
 
 createContext: use when passing down properties to children, especially if there are many levels
+const SomeContext = createContext(initalValue)
+
 useContext: use to grab context of some parent, and use it to modify some property of current prop
+const value = useContext(SomeContext)
 */
 function App() {
   return (
